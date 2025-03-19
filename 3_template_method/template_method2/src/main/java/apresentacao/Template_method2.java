3cb26d2970f952b1bcbf5699fac73265d7af3374
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-03-19 19:49:49 -0300

    atualizando

--- 3_template_method/template_method2/src/main/java/apresentacao/Template_method2.java
+++ 3_template_method/template_method2/src/main/java/apresentacao/Template_method2.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.template_method;
+package apresentacao;
 
 /**
  *
  * @author iapereira
  */
-public class Template_method {
+public class Template_method2 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
