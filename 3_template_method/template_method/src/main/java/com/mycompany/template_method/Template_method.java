aa5ebe08ca361b3c5e515a6d4b330a06fce0bf60
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-03-19 19:23:54 -0300

    atualizando

---
+++ 3_template_method/template_method/src/main/java/com/mycompany/template_method/Template_method.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.template_method;
+
+/**
+ *
+ * @author iapereira
+ */
+public class Template_method {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
