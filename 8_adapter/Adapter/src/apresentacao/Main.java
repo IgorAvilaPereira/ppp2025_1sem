facec2bac9cce10387a38982357ded23e966b44f
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2025-05-14 09:08:50 -0300

    atualizando

--- 8_adapter/Adapter/src/apresentacao/Main.java
+++ 8_adapter/Adapter/src/apresentacao/Main.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package apresentacao;
 
 import negocio.Duck;
@@ -17,12 +12,7 @@ import negocio.WildTurkey;
  * @author iapereira
  */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         MallardDuck duck = new MallardDuck();
         System.out.println("Duck:");
         duck.quack();
